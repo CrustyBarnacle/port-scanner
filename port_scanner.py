02c6874e0187e943b7af011cf48c35a23c02efe8
Mario Burgos <915931+CrustyBarnacle@users.noreply.github.com>
2024-02-17 21:11:39 +0000

    Initial commit

--- port_scanner.py
+++ port_scanner.py
@@ -1,8 +1,15 @@
+#!/usr/bin/env python3
 import socket
 
 def get_open_ports(target, port_range, verbose = False):
     open_ports = []
 
+    # validate URL or IP address, else return "Error: Invalid hostname" or "Error: Invalid IP address"
+    
+    # validate port range, else exception (not required/expected in assignment)
 
-
+    # scan target:port-range
+    
+    # return confirmed open ports
+    # if `verbose = True`, the function should return a descriptive string with each port.
     return(open_ports)
